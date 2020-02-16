3b5ce6d981dd56373259a8d0a5227b138115ef03
Alex Medvedev <alexmedvedev.amdev@gmail.com>
2020-02-16 23:30:49 +0200

    Added solution

---
+++ 5-kyo-string-incrementer.js
@@ -0,0 +1,29 @@
+// Your job is to write a function which increments a string, to create a new string.
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+
+// Examples:
+// foo -> foo1
+// foobar23 -> foobar24
+// foo0042 -> foo0043
+// foo9 -> foo10
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+//Solutions:
+function incrementString(input) {
+  if (isNaN(parseInt(input[input.length - 1]))) return input + 1;
+  return input.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+    var up = parseInt(p2) + 1;
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
+
+// function incrementString(input) {
+//     return input.replace(/([0-8]?)(9*)$/, function(s, d, ns) {
+//         return +d + 1 + ns.replace(/9/g, '0');
+//       });
+// }
+
+// //Short solution
+// let incrementString = str => str.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 : 1)
