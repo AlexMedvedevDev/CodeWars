de8b5a60d1637fc4e351389425850bcba7e5f716
Alex Medvedev <alexmedvedev.amdev@gmail.com>
2020-02-24 15:00:34 +0200

    Added solution

---
+++ 8-kyo-watermelon.js
@@ -0,0 +1,17 @@
+// Given an integral number of watermelons w (1 ≤ w ≤ 100; 1 ≤ w in Haskell),
+// check whether Pete and Billy can divide the melons so that each of them gets an even amount.
+
+// Examples
+// divide(2) === false // 2 = 1 + 1
+// divide(3) === false // 3 = 1 + 2
+// divide(4) === true  // 4 = 2 + 2
+// divide(5) === false // 5 = 2 + 3
+// divide(6) === true  // 6 = 2 + 4
+// Solutions:
+// function divide(weight){
+//     return weight % 2 === 0 && weight > 2 ? true : false;
+// }
+
+function divide(weight) {
+  return weight > 2 && !(weight % 2);
+}
