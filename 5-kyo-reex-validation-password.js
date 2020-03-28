9c1e3c59ba563fc8e26148eca11c11774388dd99
Alex Medvedev <alexmedvedev.amdev@gmail.com>
2020-03-28 18:58:54 +0200

    Add solution

---
+++ 5-kyo-reex-validation-password.js
@@ -0,0 +1,14 @@
+//Discription
+// You need to write regex that will validate a password
+// to make sure it meets the following criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a number
+// Valid passwords will only be alphanumeric characters.
+
+//Solution
+function validate(password) {
+  return /^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9]{6,}$/.test(password);
+}
