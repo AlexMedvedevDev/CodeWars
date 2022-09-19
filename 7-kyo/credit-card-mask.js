6968c42afe4535b5c0ef48d95c0ae80b2e162c03
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2022-09-19 23:01:00 +0300

    Solutions added

---
+++ 7-kyo/credit-card-mask.js
@@ -0,0 +1,38 @@
+INSRTUCTIONS
+/*
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples
+"4556364607935616" --> "############5616"
+     "64607935616" -->      "#######5616"
+               "1" -->                "1"
+                "" -->                 ""
+
+// "What was the name of your first pet?"
+
+"Skippy" --> "##ippy"
+
+"Nananananananananananananananana Batman!"
+-->
+"####################################man!"
+*/
+
+SOLUTIONS
+// function maskify(cc) {
+//     let masked = '';
+//     let lastFour = cc.substr(-4);
+//     for (let i = 0; i < cc.length - 4; i++) {
+//       masked += '#';
+//     }
+//     return masked + lastFour;
+// }
+
+function maskify(cc) {
+    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+}
+
+// function maskify(cc) {
+//     return cc.replace(/.(?=....)/g, '#');
+// }
