9496a83d3db169ebeff88db09ec9f8f70ffe17ff
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2022-09-26 22:47:59 +0300

    Solutions added

---
+++ 8-kyo/convert-a-boolean-to-a-string.js
@@ -0,0 +1,24 @@
+INSTRUCTIONS
+/* 
+Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.
+*/
+
+SOLUTIONS
+function booleanToString(b){
+    return b === true ? "true" : "false";
+}
+
+// function booleanToString(b){
+//     return b.toString();
+// }
+
+// function booleanToString(b){
+//     return b ? 'true' : 'false';
+// }
+
+// function booleanToString(b){
+//     return `${b}`
+// }
+
+// const booleanToString = b => b ? 'true' : 'false';
