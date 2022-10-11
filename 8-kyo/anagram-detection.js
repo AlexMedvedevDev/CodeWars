5cf8f2b7dc533a0c2067c89179f4f4a80ad0a636
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2022-10-11 23:13:30 +0300

    Solutions added

---
+++ 8-kyo/anagram-detection.js
@@ -0,0 +1,20 @@
+INSTRUCTIONS
+/*
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+Note: anagrams are case insensitive
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+Examples
+"foefet" is an anagram of "toffee"
+"Buckethead" is an anagram of "DeathCubeK"
+*/
+
+SOLUTIONS
+function isAnagram(test, original) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+};
+
+// var isAnagram = function(test, original) {
+//     var t = test.toLowerCase().split('').sort().join('');
+//     var o = original.toLowerCase().split('').sort().join('');
+//     return (t==o)?true:false;
+// };
