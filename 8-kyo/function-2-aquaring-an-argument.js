f19cfa67f54170569699e64032731b9cc8239d8a
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2022-10-14 22:08:45 +0300

    Solutions added

---
+++ 8-kyo/function-2-aquaring-an-argument.js
@@ -0,0 +1,16 @@
+INSTRUCTIONS
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+SOLUTIONS
+
+function square(n) {
+    return n * n;
+}
+
+// const square = (n) => n * n;
+
+// function square(x) {
+//     return x ** 2;
+// }
