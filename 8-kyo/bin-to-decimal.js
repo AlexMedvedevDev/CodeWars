3a51250f72c819cea7ab148981fa449837cfcfaf
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2023-03-21 20:57:00 +0200

    Solutions added

---
+++ 8-kyo/bin-to-decimal.js
@@ -0,0 +1,8 @@
+INSTRUCTIONS
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+SOLUTIONS
+
+function binToDec(bin){
+    return parseInt(bin, 2);
+}
