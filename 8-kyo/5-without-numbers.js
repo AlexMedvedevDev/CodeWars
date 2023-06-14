f32b8513c3442406db24a62aefe535ac0016a79a
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2023-06-14 16:03:39 +0300

    Solutions added

---
+++ 8-kyo/5-without-numbers.js
@@ -0,0 +1,23 @@
+INSTRUCTIONS
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+SOLUTIONS
+function unusualFive() {
+    return '_____'.length;
+}
+
+// function unusualFive() {
+//     return "jesus".length;
+// }
+
+// unusualFive=_=>'     '.length
+
+// const unusualFive = () =>
+//   Math.hypot(Math.ceil(Math.PI), Math.floor(Math.PI));
+
+// const unusualFive = () => 'How do you do bro?'.split(' ').length
+
+// function unusualFive() {
+//     return [...'Joker'].length;
+// }
