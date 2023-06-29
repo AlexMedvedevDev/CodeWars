8df09f2a41c229edddc7bf0fbc556ae7b8601ccf
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2023-06-29 17:43:27 +0300

    Solutions added

---
+++ 8-kyo/hex-to-decimal.js
@@ -0,0 +1,9 @@
+INSTRUCTIONS
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+SOLUTIONS
+
+function hexToDec(hexString){
+    return parseInt(hexString, 16);
+}
