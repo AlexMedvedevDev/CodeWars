620d56c6d3705876eb8f78965f7f796908aaae3a
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2023-07-07 18:23:02 +0300

    Sokutions added

---
+++ 8-kyo/makeuppercase.js
@@ -0,0 +1,12 @@
+INSTRUCTIONS
+// Write a function which converts the input string to uppercase.
+
+SOLUTIONS
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// function makeUpperCase(str) {
+//     return str.split('').map(el => el.toUpperCase()).join('')
+// }
