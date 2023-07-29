b18a79d2e3e43f6a2c2b4d153c9a45b7a0420fd9
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2023-07-29 22:23:12 +0300

    Solutions added

---
+++ 8-kyo/Grasshopper-Combine-strings.js
@@ -0,0 +1,19 @@
+INSTRUCTIONS
+
+// Create a function named combineNames that accepts two parameters (first and last name). 
+// The function should return the full name.
+
+// Example:
+// combineNames('James', 'Stevens')
+// returns:
+// 'James Stevens'
+
+SOLUTIONS
+
+function combineNames(a, b){
+    return a + " " + b;
+}
+
+// const combineNames = (...names) => names.join(' ');
+
+// const combineNames = (a, b) => `${a} ${b}`;
