5abcf3c58b7c6139098e22001695cf128c7f75a7
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2023-08-09 17:54:07 +0300

    Solutions added

---
+++ 8-kyo/is-it-a-number.js
@@ -0,0 +1,27 @@
+INSTRUCTIONS
+
+// Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+// Valid examples, should return true:
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should return false:
+
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+SOLUTIONS
+
+isDigit = s => s == parseFloat(s);
+
+// function isDigit(s) {
+//     return s==parseFloat(s);
+// }
+
+// function isDigit(s) {
+//     return parseFloat(s) === Number(s)
+// }
+
+// const isDigit = s => /^-?\d+(\.\d+)?$/.test(s);
